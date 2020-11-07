245eea2e24df83259b8e32bfdf52bc58cdaaa61e
Eina Onting <einers@outlook.com>
2020-11-06 23:10:08 -0800

    add versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :todos do
-    resources :items
+
+  # namespace without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
