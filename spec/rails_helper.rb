a4a0659bce77f4f56535c7fae9d8e55db66af87e
Eina Onting <einers@outlook.com>
2020-11-06 22:50:35 -0800

    add jwt token authorization

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,7 +72,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
